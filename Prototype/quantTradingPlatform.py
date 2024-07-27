ab7c9da0f1ee5026360f3b8952ff03b973563d80
harsh <harsh@mailtime.com>
2024-07-27 22:40:10 +0800

    added ability in quant trading platform to store submitted orders, returned executions and executed orders in memory while also logging the values

--- Prototype/quantTradingPlatform.py
+++ Prototype/quantTradingPlatform.py
@@ -24,6 +24,16 @@ class TradingPlatform:
         t_exec = threading.Thread(name='platform.on_exec', target=self.handle_execution, args=(exchSim_2_platform_execution_q, ))
         t_exec.start()
 
+        # store submitted order submissions by ID to object mapping
+        # to save initial order submission info
+        # e.g. {orderID : SingleStockOrder}
+        self.order_submissions = {}
+
+        # store order and execution by ID
+        # to save order execution info along with updated status
+        # e.g. {orderID : {order : SingleStockOrder, execution : SingleStockExecution}}
+        self.order_status_and_executions = {}
+
     def consume_marketData(self, platform_2_exchSim_order_q, marketData_2_platform_q):
         print('[%d]Platform.consume_marketData' % (os.getpid(),))
         while True:
@@ -34,13 +44,38 @@ class TradingPlatform:
             if result is None:
                 pass
             else:
+                # store order into memory 
+                self.order_submissions[result.orderID] = result
+
+                # store logs into local logs called submitted_orders.txt
+                with open('submitted_orders.txt', 'a') as f:
+                    f.write(str(result.outputAsArray()) + '\n')
+
+
                 #do something with the new order
                 platform_2_exchSim_order_q.put(result)
     
     def handle_execution(self, exchSim_2_platform_execution_q):
         print('[%d]Platform.handle_execution' % (os.getpid(),))
         while True:
-            execution = exchSim_2_platform_execution_q.get()
+            # get both execution and order
+            [execution,order] = exchSim_2_platform_execution_q.get()
+
+            # store execution into memory
+            self.order_status_and_executions[order.orderID] = {"order" : order, "execution" : execution}
+
             print('[%d] Platform.handle_execution' % (os.getpid()))
-            print(execution.outputAsArray())
+
+
+            print('------ EXECUTION',execution.outputAsArray())
+            print('------ ORDER',order.outputAsArray())
+
+            # save exeuction to local logs called executions.txt
+            with open('executions.txt', 'a') as f:
+                f.write(str(execution.outputAsArray()) + '\n')
+
+            with open('executed_orders.txt', 'a') as f:
+                f.write(str(order.outputAsArray()) + '\n')
+
+
             self.quantStrat.run(None, execution)
