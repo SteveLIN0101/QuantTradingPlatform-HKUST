dd787a1f9d16c55ac2088d99db501850b06be82a
SteveLIN0101 <143522329+SteveLIN0101@users.noreply.github.com>
2024-07-30 18:24:07 +0800

    Update SingleStockExecution.py
    Slight changes to be compact with the exchange simulator.

--- Prototype/common/SingleStockExecution.py
+++ Prototype/common/SingleStockExecution.py
@@ -6,17 +6,31 @@ Created on Sat Jul 3 07:11:28 2019
 """
 
 class SingleStockExecution():
-    
-    def __init__(self, ticker, date, timeStamp):
-        self.execID = 0
-        self.orderID = 0
+
+    def __init__(self,
+                 ticker,
+                 date,
+                 timeStamp,
+                 direction,
+                 execID,
+                 orderID,
+                 size=0,  # executed size
+                 price=None,
+                 order_status=None,
+                 rest_size=0):
+        self.execID = execID
+        self.orderID = orderID
         self.ticker = ticker
         self.date = date
         self.timeStamp = timeStamp
-        self.direction = None
-        self.price = None
-        self.size = None
-        self.comm = 0 #commission for this transaction
+        self.direction = direction  # "BI", "SI", "ACK", "Cancel Successfully But Partially Filled"
+        # "Cancel Failed fully filled", "Cancel Successfully not filled"
+        self.price = price
+        self.size = size
+        self.comm = 0  # commission for this transaction
+
+        self.order_status = order_status  # "Filled" "Partially Filled" "not filled"
+        self.rest_size = rest_size
 
     def outputAsArray(self):
         output = []
